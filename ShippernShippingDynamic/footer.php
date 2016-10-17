1105a163c8f2794cd978b926579d8e41c79c31de
Øystein Barstad <oebars@gmail.com>
2016-10-17 09:04:30 +0200

    php

---
+++ ShippernShippingDynamic/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Shippern shipping</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="normalize.css">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    </head>
+    <body>
+        <footer>
+            <p>Contact: shippernshipping@shippern.no</p> 
+            <p>phone: +47 12345678</p>
+            <p>Location: Ålesund</p>
+            <p>This webpage is protected by copyright law</p>
+        </footer>
+    </body>
+</html>
