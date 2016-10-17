d8bc66add44dcc17e6d8f2a6813f544486280aff
Øystein Barstad <oebars@gmail.com>
2016-10-17 10:48:41 +0200

    More files

---
+++ ShippernShippingDynamic/ShippernShippingDynamic/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Shippern shipping</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="normalize.css">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>
+                Shippern Shipping
+            </h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href ="index.php">Home</a>
+                    </li>
+                    
+                    <li>
+                        <a href="pickUserPage.php">Login</a>
+                    </li>
+                    
+                    <li>
+                        <a href="ourFleet.php">Our fleet</a>
+                    </li>
+                    
+                    <li>
+                        <a href="forInvestors.php">For investors</a>
+                    </li>
+                    
+                    <li>
+                        <a href="aboutUs.php">About us</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
---
+++ ShippernShippingDynamic/ShippernShippingDynamic/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Shippern shipping</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="normalize.css">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    </head>
+    <body>
+        <?php include 'header.php';?>
+        <div id="login">
+        <img src="ShippernShipping.png" alt="Company logo" style="width:304px;height:228px;">
+        </div>
+        <?php include 'footer.php';?>
+    </body>
+</html>
